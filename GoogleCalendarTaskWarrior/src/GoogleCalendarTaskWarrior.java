fd571f8a18e8df90bdf9bb10c40fdce99738305b
sarnobat <ss401533@gmail.com>
2014-01-15 12:54:46 -0800

    Update README.md

--- GoogleCalendarTaskWarrior/src/GoogleCalendarTaskWarrior.java
+++ GoogleCalendarTaskWarrior/src/GoogleCalendarTaskWarrior.java
@@ -1,15 +1,14 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
 import java.util.List;
 
-import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
@@ -21,7 +20,8 @@ public class GoogleCalendarTaskWarrior {
 
 	private static Calendar client;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException,
+			GeneralSecurityException {
 		client = getCalendarService();
 		@SuppressWarnings("unchecked")
 		List<CalendarListEntry> allCalendars = (List<CalendarListEntry>) client
@@ -33,60 +33,36 @@ public class GoogleCalendarTaskWarrior {
 		}
 	}
 
-	private static Calendar getCalendarService() {
-		final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static Calendar getCalendarService()
+			throws GeneralSecurityException, IOException {
+		HttpTransport httpTransport = GoogleNetHttpTransport
+				.newTrustedTransport();
 
-		try {
-			HttpTransport httpTransport = GoogleNetHttpTransport
-					.newTrustedTransport();
-			FileDataStoreFactory dataStoreFactory = new FileDataStoreFactory(
-					new java.io.File(System.getProperty("user.home"),
-							".store/calendar_sample"));
+		client = new Calendar.Builder(
+				httpTransport,
+				JacksonFactory.getDefaultInstance(),
+				new AuthorizationCodeInstalledApp(
+						new GoogleAuthorizationCodeFlow.Builder(
+								httpTransport,
+								JacksonFactory.getDefaultInstance(),
+								GoogleClientSecrets.load(
+										JacksonFactory.getDefaultInstance(),
+										new InputStreamReader(
+												GoogleCalendarTaskWarrior.class
+														.getResourceAsStream("/client_secrets.json"))),
+								ImmutableSet.of(CalendarScopes.CALENDAR,
+										CalendarScopes.CALENDAR_READONLY))
+								.setDataStoreFactory(
+										new FileDataStoreFactory(
+												new java.io.File(
+														System.getProperty("user.home"),
+														".store/calendar_sample")))
+								.build(), new LocalServerReceiver())
+						.authorize("user")).setApplicationName(
+				"gcal-task-warrior").build();
 
-			Credential credential;
-			{
-				{
-					// load client secrets
-					GoogleClientSecrets clientSecrets = GoogleClientSecrets
-							.load(JSON_FACTORY,
-									new InputStreamReader(
-											GoogleCalendarTaskWarrior.class
-													.getResourceAsStream("/client_secrets.json")));
-					if (clientSecrets.getDetails().getClientId()
-							.startsWith("Enter")
-							|| clientSecrets.getDetails().getClientSecret()
-									.startsWith("Enter ")) {
-						System.out
-								.println("Overwrite the src/main/resources/client_secrets.json file with the client secrets file "
-										+ "you downloaded from the Quickstart tool or manually enter your Client ID and Secret "
-										+ "from https://code.google.com/apis/console/?api=calendar#project:319518489594 "
-										+ "into src/main/resources/client_secrets.json");
-						System.exit(1);
-					}
-
-					GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-							httpTransport, JSON_FACTORY, clientSecrets,
-							ImmutableSet.of(CalendarScopes.CALENDAR,
-									CalendarScopes.CALENDAR_READONLY))
-							.setDataStoreFactory(dataStoreFactory).build();
-
-					credential = new AuthorizationCodeInstalledApp(flow,
-							new LocalServerReceiver()).authorize("user");
-				}
-			}
-
-			client = new Calendar.Builder(httpTransport, JSON_FACTORY,
-					credential).setApplicationName("gcal-task-warrior").build();
-
-			return client;
-
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-		System.exit(-1);
 		return client;
+
 	}
 
 }
