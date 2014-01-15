e03c7225138bad3864032443b930ba09ea08ad0b
sarnobat <ss401533@gmail.com>
2014-01-15 12:54:46 -0800

    Update README.md

--- GoogleCalendarTaskWarrior/src/Test.java
+++ GoogleCalendarTaskWarrior/src/Test.java
@@ -1,24 +1,8 @@
-/*
- * Copyright (c) 2013 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import org.json.JSONArray;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -29,46 +13,27 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.DataStoreFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.CalendarListEntry;
 
-/**
- * Main class for the Calendar API command line sample. Demonstrates how to make
- * an authenticated API call using OAuth 2 helper classes.
- */
 public class Test {
 
-	/**
-	 * Be sure to specify the name of your application. If the application name
-	 * is {@code null} or blank, the application will log a warning. Suggested
-	 * format is "MyCompany-ProductName/1.0".
-	 */
 	private static final String APPLICATION_NAME = "gcal-task-warrior";
 
-	/** Directory to store user credentials. */
 	private static final java.io.File DATA_STORE_DIR = new java.io.File(
 			System.getProperty("user.home"), ".store/calendar_sample");
 
-	/**
-	 * Global instance of the {@link DataStoreFactory}. The best practice is to
-	 * make it a single globally shared instance across your application.
-	 */
 	private static FileDataStoreFactory dataStoreFactory;
 
-	/** Global instance of the JSON factory. */
 	private static final JsonFactory JSON_FACTORY = JacksonFactory
 			.getDefaultInstance();
 
-	/** Global instance of the HTTP transport. */
 	private static HttpTransport httpTransport;
 
-	@SuppressWarnings("unused")
 	private static Calendar client;
 
-	/** Authorizes the installed application to access user's protected data. */
 	private static Credential authorize() throws Exception {
 		// load client secrets
 		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
@@ -86,17 +51,6 @@ public class Test {
 			System.exit(1);
 		}
 
-		// Set up authorization code flow.
-		// Ask for only the permissions you need. Asking for more permissions
-		// will
-		// reduce the number of users who finish the process for giving you
-		// access
-		// to their accounts. It will also increase the amount of effort you
-		// will
-		// have to spend explaining to users what you are doing with their data.
-		// Here we are listing all of the available scopes. You should remove
-		// scopes
-		// that you are not actually using.
 		Set<String> scopes = new HashSet<String>();
 		scopes.add(CalendarScopes.CALENDAR);
 		scopes.add(CalendarScopes.CALENDAR_READONLY);
@@ -104,7 +58,7 @@ public class Test {
 		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
 				httpTransport, JSON_FACTORY, clientSecrets, scopes)
 				.setDataStoreFactory(dataStoreFactory).build();
-		// authorize
+
 		return new AuthorizationCodeInstalledApp(flow,
 				new LocalServerReceiver()).authorize("user");
 	}
@@ -141,8 +95,9 @@ public class Test {
 			List<CalendarListEntry> object = (List<CalendarListEntry>) client
 					.calendarList().list().execute().get("items");
 			for (CalendarListEntry aCalendar : object) {
-				System.out.println(aCalendar.getSummary() + "::" + aCalendar.getId() + "::"
-						+ aCalendar.getClass() + "::" + aCalendar);
+				System.out.println(aCalendar.getSummary() + "::"
+						+ aCalendar.getId() + "::" + aCalendar.getClass()
+						+ "::" + aCalendar);
 			}
 			System.out.println("Items: \t\t" + object);
 			for (Object o : client.calendarList().list().execute().keySet()) {
