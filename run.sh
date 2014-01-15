319b19de07ec5ab7965ce74f50b0cbf1df10ac54
sarnobat <ss401533@gmail.com>
2014-01-15 12:54:46 -0800

    Update README.md

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 # export GMAIL_PASSWORD=password123 && sh run.sh
 
+groovy list.groovy   
+groovy list_calendars.groovy | column -t -s "::" | sort -h
+
 groovy gcaltaskwarrior.groovy | column -t -s "::" | sort -h
-groovy mail.groovy   
+#groovy mail.groovy   
