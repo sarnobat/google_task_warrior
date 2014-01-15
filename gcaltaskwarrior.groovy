5227c00888fe144ceaae707d8e3f935f605f5707
sarnobat <ss401533@gmail.com>
2014-01-15 12:54:46 -0800

    Update README.md

---
+++ gcaltaskwarrior.groovy
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.JSONObject;
+
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.CalendarListEntry;
+import com.google.common.collect.ImmutableSet;
+
+public class GoogleCalendarTaskWarrior {
+
+	public static void main(String[] args) throws IOException,
+			GeneralSecurityException {
+		Calendar client = getCalendarService();
+		@SuppressWarnings("unchecked")
+		List<CalendarListEntry> allCalendars = (List<CalendarListEntry>) client
+				.calendarList().list().execute().get("items");
+
+		for (CalendarListEntry aCalendar : allCalendars) {
+
+			System.out.println(aCalendar.getSummary() + " :: "
+					+ aCalendar.getId() + " :: "
+					+ aCalendar.toPrettyString() + " :: "
+					+ new JSONObject(aCalendar) + "\n");
+		}
+	}
+
+	private static Calendar getCalendarService()
+			throws GeneralSecurityException, IOException {
+		HttpTransport httpTransport = GoogleNetHttpTransport
+				.newTrustedTransport();
+		Calendar client = new Calendar.Builder(
+				httpTransport,
+				JacksonFactory.getDefaultInstance(),
+				new AuthorizationCodeInstalledApp(
+						new GoogleAuthorizationCodeFlow.Builder(
+								httpTransport,
+								JacksonFactory.getDefaultInstance(),
+								GoogleClientSecrets.load(
+										JacksonFactory.getDefaultInstance(),
+										new InputStreamReader(
+												GoogleCalendarTaskWarrior.class
+														.getResourceAsStream("/client_secrets.json"))),
+								ImmutableSet.of(CalendarScopes.CALENDAR,
+										CalendarScopes.CALENDAR_READONLY))
+								.setDataStoreFactory(
+										new FileDataStoreFactory(
+												new java.io.File(
+														System.getProperty("user.home"),
+														".store/calendar_sample")))
+								.build(), new LocalServerReceiver())
+						.authorize("user")).setApplicationName(
+				"gcal-task-warrior").build();
+
+		return client;
+
+	}
+
+}
+
