955860f17c975736cc48ec998d0f8f72d62174a3
sarnobat <ss401533@gmail.com>
2014-01-15 12:54:46 -0800

    Update README.md

--- eclipse_project/src/main/java/Test.java
+++ eclipse_project/src/main/java/Test.java
@@ -1,3 +1,9 @@
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.*;
+
 public class Test {
 	public static void main(String[] args) {
 
