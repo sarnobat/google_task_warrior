9ab982549b0facfca1f0f42887f42b90b1bb7998
Sridhar Sarnobat <ss401533@gmail.com>
2014-03-31 18:45:47 -0700

    Merge branch 'master' of https://github.com/sarnobat/google_task_warrior

--- list.sh
+++ list.sh
@@ -1,3 +1,4 @@
+cp .groovy/lib/*jar  ~/.groovy/lib/
 mv ~/.groovy/lib/json-20090211.jar ~/.groovy/lib/json-20090211.jar.disabled 2> /dev/null
 groovy list_display.groovy
 #groovy list_update.groovy &
